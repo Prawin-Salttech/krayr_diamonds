3e4ced3c2829b46f7a346bb7423caf26edaf4ffb
prawin <prawin.j07@gmail.com>
2024-09-03 19:11:55 +0530

    Completed Upto DropDown in the Home Page

--- app/page.js
+++ app/page.js
@@ -1,113 +1,67 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+
+      {/* Frame 28036 */}
+      <div className="relative bg-palevioletred h-8 flex flex-row items-center justify-center py-[10px] px-[529px] h-[32px] w-[1440px] gap-[10px] box-border text-left text-[12px] text-white font-montserrat">
+        <div className="flex flex-row items-center justify-center py-0 px-[492.5px] w-[1395px] h-[15px] gap-[10px]">
+          <div className="relative uppercase w-[410px] h-[15px] leading-[14.63px]">
+            <b>ENDS SOON!</b>
+            <span className="font-medium"> Free Diamond Jewellery With All Purchases. </span>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Frame 28037 */}
+
+      <div className="w-full relative bg-maroon flex flex-col items-start justify-start py-[16px] px-[80px] gap-[10px] box-border text-left text-[12px] text-white font-josefin-sans">
+        <div className="w-[1280.2px] relative h-[56px]">
+          <div className="absolute top-[0px] left-[0px] flex flex-row items-center justify-start gap-[302px] ">
+            <div className="w-[720.17px] h-[56px] flex flex-row items-center justify-between">
+              <div className="w-[72px] h-[24px] flex flex-row items-center justify-between">
+                <img className="w-6 relative h-6 overflow-hidden shrink-0" alt="" src="Frame.png" />
+                <img className="w-6 relative h-6 overflow-hidden shrink-0" alt="" src="Instagram.svg" />
+              </div>
+              <img className="w-[160.2px] relative h-[56px] object-cover" alt="" src="assests_teamcoachingAsset 1@4x.png" />
+            </div>
+            <div className="w-[258px] h-[24px] flex flex-row items-center justify-start gap-[30px]">
+              <div className="w-[156px] h-[24px] flex flex-row items-center justify-start gap-[20px]">
+                <img className="w-6 relative h-6 overflow-hidden shrink-0" alt="" src="search-md.svg" />
+                <img className="w-6 relative h-6 overflow-hidden shrink-0" alt="" src="user-03.svg" />
+                <img className="w-6 relative h-6 overflow-hidden shrink-0" alt="" src="heart.svg" />
+                <img className="w-6 relative h-6 overflow-hidden shrink-0" alt="" src="shopping-bag-02.svg" />
+              </div>
+              <div className="w-[72px] h-[24px] flex flex-row items-center justify-start gap-[4px]">
+                <div className="w-[56px] h-[24px] flex flex-row items-center justify-start gap-[6px]">
+                  <img className="w-6 relative h-6" alt="" src="Mask group.svg" />
+                  <div className="w-[26px] h-[18px] relative leading-[18px] uppercase font-normal">AED</div>
+                </div>
+                <img className="w-3 relative h-3 overflow-hidden shrink-0" alt="" src="Chevron down.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Dropdown Component */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full relative h-[60px] text-left text-sm text-black font-montserrat">
+        <div className="absolute top-[20px] left-[calc(50%_-_720px)] w-[1440px] h-[20px]">
+          <div className="absolute top-[0px] left-[calc(50%_-_720px)] w-[1440px] flex flex-row items-center justify-start py-0 px-[259px] box-border gap-[48px]">
+            <div className="w-[155px] h-[20px] relative leading-[20px] uppercase font-semibold whitespace-nowrap">Engagement Rings</div>
+            <div className="w-[132px] h-[20px] relative leading-[20px] uppercase font-semibold whitespace-nowrap">Wedding Bands</div>
+            <div className="w-[84px] h-[20px] relative leading-[20px] uppercase font-semibold whitespace-nowrap">Diamonds</div>
+            <div className="w-[87px] h-[20px] relative leading-[20px] uppercase font-semibold whitespace-nowrap">Customize</div>
+            <div className="w-[88px] h-[20px] relative leading-[20px] uppercase font-semibold whitespace-nowrap">Jewellery</div>
+            <div className="w-[42px] h-[20px] relative leading-[20px] uppercase font-semibold whitespace-nowrap">Gifts</div>
+            <div className="w-[76px] h-[20px] relative leading-[20px] uppercase font-semibold whitespace-nowrap">About Us</div>
+          </div>
+        </div>
       </div>
-    </main>
+
+
+
+    </>
+
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,26 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors:{
+      'palevioletred': '#a63e60',
+      'white': '#fff',
+      'maroon': '#4a1521',
+      'black': '#000000',
+      'green': '#149C48',
+      'red': '#EB1C26',
+      'lightgray': '#F0F0F0',
+    },
+    spacing:{},
+    fontFamily:{
+      "montserrat": "Montserrat",
+      "josefin-sans": "'Josefin Sans'"
+    },
+    fontSize:{
+      "inherit": "inherit"
+    }
+  },
+  corePlugins:{
+    preflight: false
   },
   plugins: [],
 };
